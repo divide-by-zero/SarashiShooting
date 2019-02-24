d2ad06ea08f560e5ed98eecb82a566289b39cdb6
vailKAKO <>
2019-02-24 16:24:31 +0900

    シングルトンの片鱗を理解できたあたり HierarchyにGameObjectとしての実体がなくても、どこかでクラス名.Instanceを呼べばいい

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using UniRx.Async;
+
+public class GameManager : MonoBehaviour
+{
+    public static float Dt;
+    public ReactiveProperty<int> clickProperty = new ReactiveProperty<int>(0);
+
+    private Subject<int> _clickSubject = new Subject<int>();
+    private static GameManager _instance;
+
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var obj = new GameObject(typeof(GameManager).Name);
+                _instance = obj.AddComponent<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        //clickProperty.Subscribe(val => { Debug.Log(val); });
+    }
+
+    public IObservable<int> OnClicked
+    {
+        get { return _clickSubject; }
+    }
+
+    private void Update()
+    {
+        Dt = Time.deltaTime;
+        //what is Value???
+        //clickProperty.Value = (int) Input.GetAxisRaw("Fire1");
+        // Debug.Log((int) Input.GetAxisRaw("Fire1"));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _clickSubject.OnNext(1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Timeline;
+using static GameManager;
+
+namespace PlayerSpace
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private const float MoveSpeed = 20;
+        private const float RotateSpeed = 200;
+        private GameManager _gameManager;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(GameManager.Instance);
+        }
+
+        private void Update()
+        {
+            PlayerMove();
+            PlayerRotate();
+        }
+
+        private void PlayerMove()
+        {
+            var speedSub = MoveSpeed * Dt;
+            var vX = Input.GetAxis("Horizontal") * speedSub;
+            var vZ = Input.GetAxis("Vertical") * speedSub;
+            transform.Translate(vX, 0, vZ);
+        }
+
+        private void PlayerRotate()
+        {
+            var mouseInputX = Input.GetAxis("Mouse X");
+            transform.RotateAround(gameObject.transform.position, Vector3.up,
+                mouseInputX * Time.deltaTime * RotateSpeed);
+        }
+    }
+}
