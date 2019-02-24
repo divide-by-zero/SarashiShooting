35e808b8ea3192c76b2f895ca8663a089aafda1c
vailKAKO <>
2019-02-24 21:48:50 +0900

    パーティクルの当たり判定追加、Tagで判断して相手に影響を及ぼす（今回はDestroy）

---
+++ Assets/Scripts/BulletCollisionContoroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollisionContoroller : MonoBehaviour
+{
+    private void OnParticleCollision(GameObject ot)
+    {
+        if (ot.CompareTag("MovingEnemy"))
+        {
+            Destroy(ot.gameObject);
+        }
+    }
+}
