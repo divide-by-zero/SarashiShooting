42b56beecf4f9e1efd9ea10b2f27c417aff1e846
vailKAKO <>
2019-02-26 01:23:39 +0900

    動く敵の体力・消去実装。速さ（色）によって体力が違うようにした

--- Assets/Scripts/BulletCollisionContoroller.cs
+++ Assets/Scripts/BulletCollisionContoroller.cs
@@ -7,12 +7,6 @@ public class BulletCollisionContoroller : MonoBehaviour
 {
     private void OnParticleCollision(GameObject ot)
     {
-        if (ot.CompareTag("MovingEnemy"))
-        {
-            enemyNum--;
-            Destroy(ot.gameObject);
-        }
-
         if (ot.CompareTag("ShootingEnemy"))
         {
             shootingEnemyNum--;
--- Assets/Scripts/EnemyMoveScript.cs
+++ Assets/Scripts/EnemyMoveScript.cs
@@ -9,12 +9,14 @@ public class EnemyMoveScript : MonoBehaviour
     private int _variation;
     private const float Speed = 0.05f;
     private const float DamageC = 500;
+    private int _movingEnemyHP;
 
     private void Awake()
     {
         _variation = Random.Range(0, 256);
 
         GetComponent<Renderer>().material.color = new Color32(255, (byte) (256 - _variation), 0, 128);
+        _movingEnemyHP = (int) ((_variation / 50) + 1);
     }
 
     private void Update()
@@ -25,11 +27,25 @@ public class EnemyMoveScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision ot)
     {
+        Debug.Log(ot.gameObject.tag);
         if (ot.gameObject.CompareTag("Player"))
         {
             PlayerInstance._playerHP -= (int) (DamageC / _variation);
-            Debug.Log((int) (DamageC / _variation));
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnParticleCollision(GameObject ot)
+    {
+        if (ot.gameObject.CompareTag("PlayerBullet"))
+        {
+            _movingEnemyHP--;
+            Debug.Log(_movingEnemyHP);
+            if (_movingEnemyHP < 1)
+            {
+                enemyNum--;
+                Destroy(this.gameObject);
+            }
         }
     }
 }
