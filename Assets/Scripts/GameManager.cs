5444844be7ae2cbe68847b567cb93a962b44cca7
ksz <divide.by.zero.divide.by.zero@gmail.com>
2019-03-02 04:49:57 +0900

    GameManagerのシングルトンをすでにシーンに存在していても良いように変更 敵残数とダメージを画面表示(仮)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,16 @@
-﻿using UnityEngine;
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private Text _leftEnemyLabel;
+
+    [SerializeField]
+    private Text _damageLabel;
+
     public int shootingEnemyNum = 0;
     public int enemyNum = 80;
     public int damageNum = 0;
@@ -13,11 +22,29 @@ public class GameManager : MonoBehaviour
         {
             if (_instance == null)
             {
-                var obj = new GameObject(typeof(GameManager).Name);
-                _instance = obj.AddComponent<GameManager>();
+                //よくあるシングルトン　すでにシーンに存在する場合はそれを使用するう
+                _instance = FindObjectOfType<GameManager>();
+                if (_instance == null)
+                {
+                    var obj = new GameObject(typeof(GameManager).Name);
+                    _instance = obj.AddComponent<GameManager>();
+                }
             }
 
             return _instance;
+        }
+    }
+
+    public void Start()
+    {
+        //enemyNumやdamageNumの変更を監視。　そのままTextへ流し込み
+        if (_leftEnemyLabel != null)
+        {
+            this.ObserveEveryValueChanged(manager => manager.enemyNum).SubscribeToText(_leftEnemyLabel);
+        }
+        if (_damageLabel != null)
+        {
+            this.ObserveEveryValueChanged(manager => manager.damageNum).SubscribeToText(_damageLabel);
         }
     }
 
